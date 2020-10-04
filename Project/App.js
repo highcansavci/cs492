4488f99f1a2e77aa7e518922e04147e765a00367
osmancan19 <osman.yildiz@ug.bilkent.edu.tr>
2020-10-04 18:23:47 +0300

    last version

--- Project/App.js
+++ Project/App.js
@@ -1,25 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{Component} from 'react';
-import { SafeAreaView,StyleSheet, Text, View, Image, TextInput,Button } from 'react-native';
+import { SafeAreaView,StyleSheet, Text, View, Image, TextInput,Button,KeyboardAvoidingView  } from 'react-native';
 import { render } from 'react-dom';
 
 export default function App() {
   
     return (
-      <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
         <SafeAreaView  style={styles.container}>
           <Image source={require('./assets/logo.png')} style={styles.logo} /> 
           <Text style={styles.title} >Bil-Events</Text>
           <TextInput style={styles.idPass} placeholder={'ID'} />
           <TextInput style={styles.idPass} placeholder={'Password'} secureTextEntry={true}/>
         </SafeAreaView >
-        
+
         <SafeAreaView  style={styles.container2}>
           <Button title = "   Log In   " style={styles.button} onPress = {() =>console.log("login tabbed")}/>
           <Button title = "   Sign In  " style={styles.button} onPress = {() =>console.log("signin tabbed")}/>
-        </SafeAreaView>
-        
-      </SafeAreaView>
+        </SafeAreaView>        
+      </KeyboardAvoidingView>
     );
 }
 
@@ -38,6 +37,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems: 'baseline',
+    
   },
   logo: {
     width: 300,
@@ -60,9 +60,5 @@ const styles = StyleSheet.create({
     width: 80,
    
   },
-  space:{
-    
-    
-  }
   
 });
