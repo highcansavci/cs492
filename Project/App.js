f16359cea1452d53485a2ea8831c6cb0717b69d3
osmancan19 <osman.yildiz@ug.bilkent.edu.tr>
2020-10-04 20:06:09 +0300

    navigation added

--- Project/App.js
+++ Project/App.js
@@ -1,64 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{Component} from 'react';
-import { SafeAreaView,StyleSheet, Text, View, Image, TextInput,Button,KeyboardAvoidingView  } from 'react-native';
-import { render } from 'react-dom';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  
-    return (
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <SafeAreaView  style={styles.container}>
-          <Image source={require('./assets/logo.png')} style={styles.logo} /> 
-          <Text style={styles.title} >Bil-Events</Text>
-          <TextInput style={styles.idPass} placeholder={'ID'} />
-          <TextInput style={styles.idPass} placeholder={'Password'} secureTextEntry={true}/>
-        </SafeAreaView >
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
-        <SafeAreaView  style={styles.container2}>
-          <Button title = "   Log In   " style={styles.button} onPress = {() =>console.log("login tabbed")}/>
-          <Button title = "   Sign In  " style={styles.button} onPress = {() =>console.log("signin tabbed")}/>
-        </SafeAreaView>        
-      </KeyboardAvoidingView>
-    );
+const Stack = createStackNavigator();
+
+function NavStack() {
+  return (
+     <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#621FF7',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle :{
+            fontWeight: 'bold',
+          },
+        }}
+      >
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={{ title: 'Login' }}
+      />
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />
+    </Stack.Navigator>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container2: {
-    flex: 0.28,
-    width: 250,
-    overflow: 'hidden',
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'baseline',
-    
-  },
-  logo: {
-    width: 300,
-    height: 300,
-    marginTop: 20,
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: "bold",
-    marginTop: 20,
-  },
-  idPass:{
-    color:'white',
-    width: 230,
-    height: 40,
-    backgroundColor: '#737373',
-    marginTop: 15,
-  },
-  button: {
-    width: 80,
-   
-  },
-  
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <NavStack />
+    </NavigationContainer>
+  );
+}
---
+++ Project/pages/Login.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import React,{Component} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView,StyleSheet, Text, View, Image, TextInput,Button,KeyboardAvoidingView,  } from 'react-native';
+import { render } from 'react-dom';
+
+class Login extends Component{
+  render() {
+    return (      
+      <SafeAreaView style={styles.container} >
+        <SafeAreaView  style={styles.container} > 
+          <Image source={require('./assets/logo.png')} style={styles.logo} /> 
+          <Text style={styles.title} >Bil-Events</Text>
+          <TextInput style={styles.idPass} placeholder={'ID'} />
+          <TextInput style={styles.idPass} placeholder={'Password'} secureTextEntry={true}/>
+        </SafeAreaView >
+
+        <SafeAreaView  style={styles.container2} >
+          <Button title = "   Login   " style={styles.button} onPress = {() =>console.log("login tabbed")}/>
+          <Button title = "   Signup  " style={styles.button} onPress = {() =>this.props.navigation.navigate('Signup')}/>
+        </SafeAreaView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    flex: 1,
+    width: 250,
+    overflow: 'hidden',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'baseline',
+    
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+  idPass:{
+    color:'white',
+    width: 230,
+    height: 40,
+    backgroundColor: '#737373',
+    marginTop: 15,
+  },
+  button: {
+    width: 80,
+   
+  },
+  
+});
+
+export default Login;
