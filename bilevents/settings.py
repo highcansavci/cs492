d571def5677033ab55a76bd73af111ad4fa901c2
Can Savcı <43791867+highcansavci@users.noreply.github.com>
2020-11-23 06:51:01 +0300

    Debug

--- bilevents/settings.py
+++ bilevents/settings.py
@@ -137,6 +137,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../bilevents/static'),
+    os.path.join(PROJECT_ROOT, '../bilevents/static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
